6d86d2c215470b5a9e941dc1c8d79ffc5f5106cf
Jori Peterson <joripeterson@gmail.com>
2019-04-30 21:05:43 -0600

    Add sorting categories method

--- lib/deck.rb
+++ lib/deck.rb
@@ -9,8 +9,18 @@ class Deck
       cards.count
     end
 
-    # def cards_in_category
-    #   cards.each do |block_variable|
-    # end
+    def cards_in_category(category)
+      # return array of the specific category called
+      ret = []
+      cards.each do |card|
+        if card.category == category
+          ret.push(card)
+        end
+      end
+
+      return ret
+
+      # cards.select{ |card| card.category == category }
+    end
 
 end
