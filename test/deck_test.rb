f16673a1461c006a81b62fae080124eb574a7a8d
Jori Peterson <joripeterson@gmail.com>
2019-04-30 18:14:12 -0600

    Commit count test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -17,4 +17,26 @@ class DeckTest < Minitest::Test
     deck = Deck.new(cards)
     assert_instance_of Card, deck.cards.first
   end
+
+  def test_that_it_has_3_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+    assert_equal 3, deck.count
+  end
+
+  def test_it_returns_the_category
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+    assert_equal [card_2, card_3], deck.cards_in_category(:STEM)
+  end
 end
