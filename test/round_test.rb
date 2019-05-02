e5985409d5661a1933a3b733a4172f2bcccaa600
Jori Peterson <joripeterson@gmail.com>
2019-05-02 15:46:56 -0600

    Update variables in round test

--- test/round_test.rb
+++ test/round_test.rb
@@ -8,34 +8,23 @@ require 'pry'
 
 class RoundTest < Minitest::Test
 
-  def test_round_works_with_deck
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    deck = Deck.new([card_1, card_2, card_3])
-    round = Round.new(deck)
+  def setup
+      @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      @deck = Deck.new([@card_1, @card_2, @card_3])
+      @round = Round.new(@deck)
+  end
 
-    assert_equal deck, round.deck
+  def test_deck_has_a_round
+      assert_equal @deck, @round.@deck
   end
 
   def test_round_starts_with_empty_turn_array
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    deck = Deck.new([card_1, card_2, card_3])
-    round = Round.new(deck)
-
-    assert_equal [], round.turns
+      assert_equal [], @round.turns
   end
 
   def test_current_card_is_first_in_array
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    deck = Deck.new([card_1, card_2, card_3])
-    round = Round.new(deck)
-
-    assert_equal card_1, round.current_card
+      assert_equal @card_1, @round.current_card
   end
-
 end
