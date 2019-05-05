36b439a47666739e1b4f3bbc0e9b41389fb24c19
Jori Peterson <joripeterson@gmail.com>
2019-05-04 18:18:32 -0600

    Add working game

--- flash_card_runner.rb
+++ flash_card_runner.rb
@@ -3,26 +3,35 @@ require './lib/turn'
 require './lib/deck'
 require './lib/round'
 
-class Game
-  attr_reader :start
-  def initialize
-    @start =
-  "Welcome! You're playing with 4 cards.
-  --------------------------------------------
-  This is card number 1 out of 4.
-  Question: What is 5 + 5"
-  end
+
+@card_1 = Card.new("What is 5 + 5?", "10", :STEM)
+@card_2 = Card.new("What is Rachel's favorite animal?", "rabbit", :Staff)
+@card_3 = Card.new("What is Mike's middle name?", "nobody knows", :Staff)
+@card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Pop_Culture)
+
+@deck = Deck.new([@card_1, @card_2, @card_3, @card_4])
+@round = Round.new(@deck)
+
+
+
+p @round.start
+p "-------------------------------"
+
+for this_deck in 0..3 do
+
+p "This is card number #{@round.turns.length + 1} out of 4."
+p "Question: #{@round.current_card.question}"
+
+@guess = gets.chomp
+my_turn = @round.take_turn(@guess)
+p my_turn.feedback
+
 end
 
-new_game = Game.new
-puts new_game.start
 
+p "****** Game Over! ******"
 
-
-
-# @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-# @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-# @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-# @deck = Deck.new([@card_1, @card_2, @card_3])
-# @round = Round.new(@deck)
-# @turn = Turn.new("Juneau", @card)
+p "You had #{@round.number_correct} correct guesses out of #{@round.turns.count} for a total score of #{@round.percent_correct}%."
+p "STEM - #{@round.percent_correct_by_category(:STEM)} correct"
+p "Turing Staff - #{@round.percent_correct_by_category(:Staff)} correct"
+p "Pop Culture - #{@round.percent_correct_by_category(:Pop_Culture)} correct"
